5450e98fab49d7a0b44aff689f105414fa78ea3c
Saad Ali Khan <saad.niaxi47@gmail.com>
2021-12-02 17:42:09 +0500

    day2

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-import { square, diag } from 'lib.js';
-console.log(square(11)); // 121
-console.log(diag(4, 3)); // 5
+class Student {
+    constructor(name, email,degree){
+        this.name = name;
+        this.email = email;
+        this.degree = degree;
+    }
+    // constructor(age){
+    //     this.age = age;
+    // }
+
+    display(){
+        for(var i=0; i<5;i++){
+        console.log(this.name,this.email,this.degree);
+    }}
+
+    underage= x => {
+        if(x<=18)
+            return true;
+        return false;
+    }
+}
+
+//const kdc = new Student('18');
+const std = new Student('asad','bcsf18a517@pucit.edu.pk','bscs18');
+std.display();
+console.log('Student is underage : '+ std.underage(18));
